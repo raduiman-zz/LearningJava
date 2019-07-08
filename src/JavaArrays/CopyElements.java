0b48ff5f027b7ca2aa881b399ad57b12d73c4914
Radu Iman <radu.iman94@gmail.com>
2019-07-08 10:37:18 +0300

    Add FindFrequency program

--- src/JavaArrays/CopyElements.java
+++ src/JavaArrays/CopyElements.java
@@ -13,11 +13,8 @@ public class CopyElements {
     public static void main(String[] args){
         BubbleSort alfa = new BubbleSort();
         int a[] = alfa.readArray();
-        Scanner input_a = new Scanner(System.in);
-
         BubbleSort beta = new BubbleSort();
         int b[] = beta.readArray();
-        Scanner input_b = new Scanner(System.in);
         Copy(a, b);
         for(int i = 0; i < a.length; i++) {
             System.out.print(b[i] + " ");
