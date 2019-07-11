49634709ed0466d07e6fe435a2f4211ab455ec3b
Radu Iman <radu.iman94@gmail.com>
2019-07-11 20:30:45 +0300

    Add Static Block example.

---
+++ src/OOP/StaticBlock.java
@@ -0,0 +1,8 @@
+package OOP;
+
+public class StaticBlock {
+    static {System.out.println("Static block invoked !");}
+    public static void main(String[] args) {
+        System.out.println("Hello Static Block");
+    }
+}
