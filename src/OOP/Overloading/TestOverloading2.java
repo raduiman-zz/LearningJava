3be3ed9f250e5c6dd5e61b9dc20cde1a0c8c429e
Radu Iman <radu.iman94@gmail.com>
2019-07-13 10:24:25 +0300

    Add TestOverloading2 class. Overloading by changing the arguments type.

---
+++ src/OOP/Overloading/TestOverloading2.java
@@ -0,0 +1,11 @@
+package OOP.Overloading;
+class Adder1 {
+    static int add(int a, int b) { return a + b; }
+    static double add(double a, double b) { return a + b; }
+}
+public class TestOverloading2 {
+    public static void main(String[] args) {
+        System.out.println(Adder1.add(11, 11));
+        System.out.println(Adder1.add(11.5, 11.8));
+    }
+}
