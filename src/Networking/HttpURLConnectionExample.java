a32ed7570fef72c2769bff5d26a70e519fc1727e
Radu Iman <radu.iman94@gmail.com>
2019-08-09 10:15:19 +0300

    Add InetAddress example

--- src/Networking/HttpURLConnectionExample.java
+++ src/Networking/HttpURLConnectionExample.java
@@ -1,17 +1,20 @@
 package Networking;
 
 import java.net.HttpURLConnection;
+import java.net.InetAddress;
 import java.net.URL;
 
 public class HttpURLConnectionExample {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://www.javatpoint.com/java-tutorial");
+            InetAddress ip = InetAddress.getByName("www.google.com");
             HttpURLConnection huc = (HttpURLConnection) url.openConnection();
             for (int i = 1; i <= 9; i++) {
                 System.out.println(huc.getHeaderFieldKey(i) + " = " + huc.getHeaderField(i));
             }
             huc.disconnect();
+            System.out.println("IP Address: " + ip.getHostAddress());
         } catch (Exception e) {
             System.out.println(e);
         }
